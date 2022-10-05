97ca73f480d21b7513aec752cbf060cf0bc72607
Caio Rigues <105236708+caio-faria-rigues@users.noreply.github.com>
2022-10-05 13:56:37 -0300

    uso de sensor flexível adaptado

--- tcc_mark_2.ino
+++ tcc_mark_2.ino
@@ -1,67 +1,67 @@
-int speed2_pin = 3;
+int speed1_pin = 3;
 
-int finger2_output1 = 2;
-int finger2_output2 = 4;
+int finger1_output1 = 2;
+int finger1_output2 = 4;
 
-int sensor2 = A1;
+int sensor1 = A0;
 
-int sensor2pos1 = 0;
+int sensor1pos1 = 0;
 
-int sensor2pos2 = 0;
+int sensor1pos2 = 0;
 
-int move2 = 0;
+int move1 = 0;
 
 int steps = 1000;
 
 //definir a potência do movimento:
-int force = 30;
+int force = 45;
 //definir a sensibilidade mínima do movimento:
 int sense = 10;
 
 void setup()
 {
 //pinos dos motores
-pinMode(speed2_pin, OUTPUT);
+pinMode(speed1_pin, OUTPUT);
   
-pinMode(finger2_output1, OUTPUT);
-pinMode(finger2_output2, OUTPUT);
+pinMode(finger1_output1, OUTPUT);
+pinMode(finger1_output2, OUTPUT);
   
 Serial.begin(9600);
 
 //pinos dos sensores
-pinMode(sensor2, INPUT);
+pinMode(sensor1, INPUT);
 }
 
 void loop()
 {
-analogWrite(speed2_pin, 300);  
+analogWrite(speed1_pin, 300);  
 
-sensor2pos1 = map(analogRead(sensor2), 0, 1023, 0, 100);
+sensor1pos1 = map(analogRead(sensor1), 14, 1020, 0, 100);
 delay(1000);
-sensor2pos2 = map(analogRead(sensor2), 0, 1023, 0, 100);
+sensor1pos2 = map(analogRead(sensor1), 0, 1023, 0, 100);
 
-//move2 é a diferença de posição 
-move2 = sensor2pos1 - sensor2pos2;
-Serial.println(move2);
+//move1 é a diferença de posição 
+move1 = sensor1pos1 - sensor1pos2;
+Serial.println(move1);
 
-  if (move2 > sense || move2 < -sense){
-      if (move2 < 0){
-          steps = move2 * -force;
+  if (move1 > sense || move1 < -sense){
+      if (move1 < 0){
+          steps = move1 * -force;
           Serial.println(steps);
-          digitalWrite(finger2_output1, HIGH);
-          digitalWrite(finger2_output2, LOW);
+          digitalWrite(finger1_output1, HIGH);
+          digitalWrite(finger1_output2, LOW);
           delay(steps);
-          digitalWrite(finger2_output1, LOW);
-          digitalWrite(finger2_output2, LOW);
+          digitalWrite(finger1_output1, LOW);
+          digitalWrite(finger1_output2, LOW);
       }
-      if (move2 > 0){
-          steps = move2 * force;
+      if (move1 > 0){
+          steps = move1 * force;
           Serial.println(steps);
-          digitalWrite(finger2_output1, LOW);
-          digitalWrite(finger2_output2, HIGH);
+          digitalWrite(finger1_output1, LOW);
+          digitalWrite(finger1_output2, HIGH);
           delay(steps);
-          digitalWrite(finger2_output1, LOW);
-          digitalWrite(finger2_output2, LOW);
+          digitalWrite(finger1_output1, LOW);
+          digitalWrite(finger1_output2, LOW);
       }
   }
 }
